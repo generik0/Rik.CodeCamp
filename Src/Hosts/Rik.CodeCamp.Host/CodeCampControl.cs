c5ecfb440037aaa1160c0878260c91c17a521183
Rik Svendsen Rose <rsr@gait.dk>
2017-03-25 14:02:44 +0100

    Add Service Host

---
+++ Src/Hosts/Rik.CodeCamp.Host/CodeCampControl.cs
@@ -0,0 +1,6 @@
+﻿namespace Rik.CodeCamp.Host
+{
+    internal class CodeCampControl
+    {
+    }
+}
---
+++ Src/Hosts/Rik.CodeCamp.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using GAIT.Utilities;
+
+namespace Rik.CodeCamp.Host
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceControl>(sc =>
+                {
+                    sc.ConstructUsing(CreateServiceControl);
+                    sc.WhenStarted((s, hostControl) => s.Start(hostControl));
+                    sc.WhenStopped((s, hostControl) => s.Stop(hostControl));
+                });
+                x.SetDescription("Hist for Rik.CodeCamp");
+                x.SetDisplayName("Rik.CodeCamp.Host");
+                x.SetServiceName("Rik.CodeCamp.Host");
+                x.UseNLog();
+            });
+        }
+
+        private static ServiceControl CreateServiceControl(HostSettings settings)
+        {
+            return ActivatorFactory.Resolve<CodeCampControl>();
+        }
+    }
+}
