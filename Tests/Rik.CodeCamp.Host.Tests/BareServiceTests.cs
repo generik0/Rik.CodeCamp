5972628057ea309a3da00da95a87ce8536ea03c5
Rik Svendsen Rose <rsr@gait.dk>
2017-03-25 20:29:13 +0100

    Fix test to start host up

--- Tests/Rik.CodeCamp.Host.Tests/BareServiceTests.cs
+++ Tests/Rik.CodeCamp.Host.Tests/BareServiceTests.cs
@@ -8,47 +8,19 @@ using Castle.Windsor;
 using FluentAssertions;
 using NUnit.Framework;
 using Rik.CodeCamp.Core.Contracts;
+using Rik.CodeCamp.Host.Tests.Helpers;
+using static Rik.CodeCamp.Host.Tests.Helpers.TestSetupFixture;
 
 namespace Rik.CodeCamp.Host.Tests
 {
     [TestFixture]
-    public class BareServiceTests
+    public class BareServiceTests : TestSetupFixture
     {
-        private static BasicHttpBinding _clientBinding;
-        private static IWindsorContainer _container;
-
-        [OneTimeSetUp]
-        public static void TestSetup()
-        {
-            var process = Process.GetProcesses();
-            if (!process.Any(x => x.ProcessName.Contains("Rik.CodeCamp.Host"))) // Sorry ran out of time.
-            {
-                Assert.Fail("Please start the host as admin....");
-            }
-            _container = new WindsorContainer();
-            _container.Kernel.AddFacility<TypedFactoryFacility>();
-            _container.Kernel.AddFacility<WcfFacility>();
-            _clientBinding = new BasicHttpBinding
-            {
-                Security =
-                {
-                    Mode = BasicHttpSecurityMode.None,
-                    Transport = {ClientCredentialType = HttpClientCredentialType.None},
-                    Message = {ClientCredentialType = BasicHttpMessageCredentialType.Certificate }
-                }
-            };
-            _container.Register(Component.For<IBarService>()
-                .AsWcfClient(new DefaultClientModel
-                {
-                    Endpoint = WcfEndpoint.BoundTo(_clientBinding).At("http://localhost/BarService")
-                }).LifestyleTransient());
-
-        }
-
+        
         [Test, Category("Integration")]
         public static void IsConnected_True()
         {
-            var target = _container.Resolve<IBarService>();
+            var target = Container.Resolve<IBarService>();
             var actual = target.IsConnected();
             actual.Should().BeTrue();
         }
---
+++ Tests/Rik.CodeCamp.Host.Tests/Helpers/TestSetupFixture.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
+using Castle.Facilities.WcfIntegration;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using Rik.CodeCamp.Core.Contracts;
+
+namespace Rik.CodeCamp.Host.Tests.Helpers
+{
+    public abstract class TestSetupFixture
+    {
+        private static BasicHttpBinding _clientBinding;
+        public static IWindsorContainer Container;
+
+        [OneTimeSetUp]
+        public static void TestSetup()
+        {
+            var process = Process.GetProcesses();
+            if (!process.Any(x => x.ProcessName.Contains("Rik.CodeCamp.Host")))
+            {
+                var paths = TestContext.CurrentContext.TestDirectory.Split('\\');
+                var testsIndex = paths.Select((path, i) => new { path, i }).First(x=>x.path.Equals("Tests")).i;
+                var hostPath = $@"{string.Join(@"\", paths.Take(testsIndex))}\Src\Hosts\Rik.CodeCamp.Host\bin\Debug\Rik.CodeCamp.Host.exe";
+                Process.Start(hostPath);
+                Task.Delay(2000).Wait();
+            }
+            if (Container != null) return;
+            Container = new WindsorContainer();
+            Container.Kernel.AddFacility<TypedFactoryFacility>();
+            Container.Kernel.AddFacility<WcfFacility>();
+            _clientBinding = new BasicHttpBinding
+            {
+                Security =
+                {
+                    Mode = BasicHttpSecurityMode.None,
+                    Transport = {ClientCredentialType = HttpClientCredentialType.None},
+                    Message = {ClientCredentialType = BasicHttpMessageCredentialType.Certificate}
+                }
+            };
+            Container.Register(Component.For<IBarService>()
+                .AsWcfClient(new DefaultClientModel
+                {
+                    Endpoint = WcfEndpoint.BoundTo(_clientBinding).At("http://localhost/BarService")
+                }).LifestyleTransient());
+
+        }
+    }
+}
